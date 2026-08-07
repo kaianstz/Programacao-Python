b27614c4dcc99de520b9297f7618513cfde8c23e
Kaian Mateus Oliveira dos Santos <kaianstsz@gmail.com>
2026-08-07 10:00:30 +0000

    Desafios da Aula 4 do guanabara

---
+++ Desafio-1-2-3-Aula1.py
@@ -0,0 +1,14 @@
+print('============ DESAFIO 01 ==================')
+nome = input ('Qual o seu nome ')
+print ('Olá', nome, '! Prazer em te Conhecer!')
+
+print('============ DESAFIO 02 ==================')
+dia = input ('Dia = ')
+mes = input ('Mes = ')
+ano = input ('Ano = ')
+print ('Voce Nasceu no', dia, 'de', mes, 'de', ano, '. Correto?' )
+
+print('============ DESAFIO 03 ==================')
+numero1 = input ('Primeiro numero ')
+numero2 = input ('Segundo numero ')
+print ('A some é,', int(numero1) + int(numero2))
---
+++ OláMundo.py
@@ -0,0 +1,9 @@
+nome = 'kaian'
+idade = 25
+peso = 75.8
+print(nome, idade, peso)
+
+nome1 = input ('Qual o seu nome? ')
+idade1 = input ('Quantos anos voce tem? ')
+peso1 = input ('Quantos quilos voce pesa? ')
+print (nome1, 'tem', idade1, 'anos e pesa', peso1, 'Quilos')
