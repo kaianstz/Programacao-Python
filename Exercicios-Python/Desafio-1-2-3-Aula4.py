8640ae6eb554dc1ad32ab0bd6209a2fad0391a79
Kaian Mateus Oliveira dos Santos <kaianstsz@gmail.com>
2026-08-11 09:29:46 +0000

    aula 06 finalizada

---
+++ Exercicios-Python/Desafio-1-2-3-Aula4.py
@@ -0,0 +1,15 @@
+print('============ DESAFIO 01 ==================')
+nome = input('Qual o seu nome: ')
+print('Olá', nome, '! Prazer em te Conhecer!')
+
+print('============ DESAFIO 02 ==================')
+dia = input('Dia: = ')
+mes = input('Mes: = ')
+ano = input('Ano: = ')
+print('Voce Nasceu no dia', dia, 'de', mes, 'de', ano, '. Correto?' )
+
+print('============ DESAFIO 03 ==================')
+numero1 = int(input('Digite o Primeiro numero: '))
+numero2 = int(input('Digite o Segundo numero: '))
+soma = numero1 + numero2
+print('A some entre {} e {} é igual a {}'.format(numero1, numero2, soma))
