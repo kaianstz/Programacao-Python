737cd80f4d4d49b166d305fe99e43aa7e3ddbed3
Kaian Mateus Oliveira dos Santos <kaianstsz@gmail.com>
2026-08-17 09:29:36 +0000

    AULA 07

---
+++ Aulas-Python/Aula-07.py
@@ -0,0 +1,9 @@
+n1 = int(input('Digite um valor: '))
+n2 = int(input('Digite outro valor: '))
+s = n1 + n2
+m = n1 * n2
+d = n1 / n2
+di = n1 // n2
+e = n1 ** n2
+print('A soma é: {} \nO produto é: {} \nA Divisão é: {:.3f}'.format(s, m, d))
+print('Divisão Inteira é: {} \nA Potencia é: {}'.format(di, e))
